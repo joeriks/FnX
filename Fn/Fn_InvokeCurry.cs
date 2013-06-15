de5d8fa6ae0fa4fab7fa00497bdad07272e20f99
Jonas Eriksson <jonas@programenta.se>
2013-06-15 10:46:44 +0200

    Added first iteration for currying

---
+++ Fn/Fn_InvokeCurry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FnX
+{
+    public partial class Fn
+    {
+        public static TOut CInvoke<T1, TOut>(this Func<T1, TOut> func, T1 t1)
+        {
+            return func(t1);
+        }
+        public static Func<T2, TOut> CInvoke<T1, T2, TOut>(this Func<T1, T2, TOut> func, T1 t1)
+        {
+            return t2 => func(t1, t2);
+        }
+        public static Func<T3, TOut> CInvoke<T1, T2, T3, TOut>(this Func<T1, T2, T3, TOut> func, T1 t1, T2 t2)
+        {
+            return t3 => func(t1, t2, t3);
+        }
+        public static Func<T2, T3, TOut> CInvoke<T1, T2, T3, TOut>(this Func<T1, T2, T3, TOut> func, T1 t1)
+        {
+            return (t2, t3) => func(t1, t2, t3);
+        }
+        public static TOut CInvoke<T1, T2, TOut>(this Func<T1, T2, TOut> func, T1 t1, T2 t2)
+        {
+            return func(t1, t2);
+        }
+        //public static Func<TOut> CInvoke<T1, T2, TOut>(this Func<T1, T2, TOut> func, T1 t1, T2 t2)
+        //{
+        //    return ()=>func(t1, t2);
+        //}
+    }
+}
---
+++ FnTests/FnTests/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using FnX;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FnTests
+{
+    [TestClass]
+    public class BasicCurryTests
+    {
+        [TestMethod]
+        public void CurryTwoToOne()
+        {
+            var function = Fn.Create((string a,string b)=>a+b);
+            var reduced = function.CInvoke("re");
+
+            var result = reduced("sult");
+            Assert.AreEqual("result",result);
+
+        }
+        [TestMethod]
+        public void CurryTwoToOneNumeric()
+        {
+            var function = Fn.Create((int a, int b) => a + b);
+            var reduced = function.CInvoke(1);
+
+            var result = reduced(1);
+            Assert.AreEqual(2,result);
+
+        }
+        [TestMethod]
+        public void CurryTwoToOneStringAndNumeric()
+        {
+            var function = Fn.Create((string a, int b) => a + b);
+            var reduced = function.CInvoke("Number");
+
+            var result = reduced(1);
+            Assert.AreEqual("Number1", result);
+
+        }
+        [TestMethod]
+        public void CurryThreeToOneNumeric()
+        {
+            var function = Fn.Create((int a, int b, int c) => a + b + c);
+            var reduced = function.CInvoke(1,2);
+
+            var result = reduced(3);
+            Assert.AreEqual(6, result);
+
+        }
+        [TestMethod]
+        public void CurryThreeToTwoNumeric()
+        {
+            var function = Fn.Create((int a, int b, int c) => a + b + c);
+            var reduced = function.CInvoke(1);
+
+            var result = reduced(1,1);
+            Assert.AreEqual(3, result);
+
+        }
+        [TestMethod]
+        public void CurryTwoToResult()
+        {
+            var function = Fn.Create((int a, int b) => a + b);
+            var result = function.CInvoke(1,1);
+
+            Assert.AreEqual(2, 2);
+
+        }    
+    }
+}
